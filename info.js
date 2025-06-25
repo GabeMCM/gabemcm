02613adede668676661bcc401a2d7744d896697e
Gabriel Mateus <123529056+GabeMCM@users.noreply.github.com>
2025-06-25 00:28:53 +0000

    [24-06] [21:29]

---
+++ info.js
@@ -0,0 +1,14 @@
+export const list = [
+    {
+        title: 'Sistema de Garantias',
+        description: 'Substitui planilhas manuais com interface funcional.',
+        tech: ['TypeScript', 'HTML', 'CSS'],
+        repo: 'https://github.com/GabeMCM/Sistema-de-Gerenciamento-de-Garantias'
+    },
+    {
+        title: 'NoobDB',
+        description: 'Mini-banco de dados em JS com IndexedDB e API semelhante a ORM.',
+        tech: ['TypeScript', 'IndexedDB'],
+        repo: 'https://github.com/GabeMCM/Noobdb'
+    }
+]
--- main.js
+++ main.js
@@ -1,22 +1,13 @@
-import { heading } from './blocks/heading.js'
-import { paragraph } from './blocks/paragraph.js'
-import { projectCard } from './blocks/projectCard.js'
+import { heading } from './components/heading.js'
+import { paragraph } from './components/paragraph.js'
+import { projectCard } from './components/projectCard.js'
+import { list } from './info.js'
 
 const app = document.getElementById('app')
 
 app.appendChild(heading('Gabriel Mateus – Portfólio'))
 app.appendChild(paragraph('Desenvolvedor focado em backend e arquitetura limpa.'))
 
-app.appendChild(projectCard({
-  title: 'Sistema de Garantias',
-  description: 'Substitui planilhas manuais com interface funcional.',
-  tech: ['TypeScript', 'HTML', 'CSS'],
-  repo: 'https://github.com/GabeMCM/Sistema-de-Gerenciamento-de-Garantias'
-}))
-
-app.appendChild(projectCard({
-  title: 'NoobDB',
-  description: 'Mini-banco de dados em JS com IndexedDB e API semelhante a ORM.',
-  tech: ['TypeScript', 'IndexedDB'],
-  repo: 'https://github.com/GabeMCM/Noobdb'
-}))
+list.forEach((l) => {
+  app.appendChild(projectCard(l))
+})
